fd16f0262b239775a3d7d4bbbfd4d4598f5770a4
Henry Zhang <pistachio@Henrys-MacBook-Neo.local>
2026-07-13 16:29:26 +0900

    changed path for glb again

--- viewer.js
+++ viewer.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const MODEL_PATH_PROJECT_1 = './tomato-is-a-fruit.github.io/public/LHRCustomScreen.glb';
-const MODEL_PATH_PROJECT_3 = './tomato-is-a-fruit.github.io/public/Cryostat_TLA.glb';
+const MODEL_PATH_PROJECT_1 = 'https://tomato-is-a-fruit.github.io/public/Cryostat_TLA.glb';
+const MODEL_PATH_PROJECT_3 = 'https://tomato-is-a-fruit.github.io/public/LHRCustomScreen.glb';
 
 function createViewer(wrapId, emptyId, modelPath){
   const wrap = document.getElementById(wrapId);
